4b1e1a5967f08538c3170e4678f83ba0908781f2
aarpitsharmaa27 <arpit8894350818@gmail.com>
2026-01-14 00:33:26 +0530

    Added double the element

---
+++ Double_the_element.py
@@ -0,0 +1,11 @@
+# by pip install numpy we can intall numpy
+
+import numpy as np
+
+print(np.__version__)
+
+
+array = np.array([1, 2, 3,4,5])
+array = array * 2  # double the values of array
+print(array)
+print(type(array))
